ecf315bb9e07a779a4ef5d2cc863968dccc14617
unknown <uxob03@outlook.com>
2021-11-03 19:24:15 -0400

    praying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { useState } from 'react';
+import {BASE_URL, API_KEY} from './index.js';
+
 
 function App() {
+  const [spaceData, setSpaceData] = useState(null)
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      setSpaceData(res.data)
+      console.log(setSpaceData)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {/* <Image spaceData ? setSpaceData(spaceData.hdurl) : null } */}
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = '3GA2pxnUkN1JVexpvPrtnyrmO0RlEzQHi6V9dFtX'
+
 ReactDOM.render(<App />, document.getElementById("root"));
