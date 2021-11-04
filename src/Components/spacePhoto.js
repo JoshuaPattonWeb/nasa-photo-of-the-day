73833938cf20df09228646a0ea419ebc3a8b6865
unknown <uxob03@outlook.com>
2021-11-03 22:37:09 -0400

    final push

--- src/Components/spacePhoto.js
+++ src/Components/spacePhoto.js
@@ -10,6 +10,7 @@ const SpacePhoto = (props) => {
             <h1>{props.data.title}</h1>
             <h2>{props.data.date}</h2>
             <img src={props.data.url} alt='Big Nebula' />
+            <h2>Image Credit & Copyright: {props.data.copyright}</h2>
             <div>
             <h2>{props.data.explanation}</h2>
             </div>
